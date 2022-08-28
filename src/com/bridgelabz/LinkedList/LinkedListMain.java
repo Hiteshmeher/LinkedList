7bf8c230171ddaebe18e3ac8f71712918bc18236
Hiteshmeher <meher.hitesh4@gmail.com>
2022-08-28 20:24:00 +0530

    [Hitesh]: Added code to insert data in between nodes

--- src/com/bridgelabz/LinkedList/LinkedListMain.java
+++ src/com/bridgelabz/LinkedList/LinkedListMain.java
@@ -13,9 +13,9 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		// creating Object of LinkedList Class
 		LinkedList list = new LinkedList();
-		list.add(56); 		// Adding 56
-		list.append(30); 	// Append 30 to 56
-		list.append(70); 	// Append 70 to 30
-		list.display(); 	// Printing the Final List
+		list.add(56); 				// Adding 56
+		list.add(70); 				// Adding 70 now 56 -> 70
+		list.insertInBetween(30); 	// Inserting 30 in between 56 and 70  
+		list.display(); 			// Printing the Final List = 56 -> 30 -> 70
 	}
 }
